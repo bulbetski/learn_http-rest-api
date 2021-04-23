7bdc28dcd3d3d7abe631cae3aa839c4d67320034
Zakhar <bulbetski42@gmail.com>
2021-04-23 18:25:24 +0300

    refactor apiserver

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,71 +1,33 @@
 package apiserver
 
 import (
-	"github.com/bulbetski/learn_http-rest-api/internal/app/store"
-	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
-	"io"
+	"database/sql"
+	"github.com/bulbetski/learn_http-rest-api/internal/app/store/sqlstore"
 	"net/http"
 )
 
-type ApiServer struct {
-	config *Config
-	logger *logrus.Logger
-	router *mux.Router
-	store  *store.Store
-}
-
-func New(config *Config) *ApiServer {
-	return &ApiServer{
-		config: config,
-		logger: logrus.New(),
-		router: mux.NewRouter(),
-	}
-}
-
-func (s *ApiServer) Start() error {
-	if err := s.configureLogger(); err != nil {
-		return err
-	}
-
-	s.configureRouter()
-
-	if err := s.configureStore(); err != nil {
-		return err
-	}
-
-	s.logger.Info("starting api server")
-
-	return http.ListenAndServe(s.config.BindAddr, s.router)
-}
-
-func (s *ApiServer) configureLogger() error {
-	level, err := logrus.ParseLevel(s.config.Loglevel)
+func Start(config *Config) error {
+	db, err := newDB(config.DatabaseUrl)
 	if err != nil {
 		return err
 	}
+	defer db.Close()
 
-	s.logger.SetLevel(level)
+	store := sqlstore.New(db)
+	srv := newServer(store)
 
-	return nil
+	return http.ListenAndServe(config.BindAddr, srv)
 }
 
-func (s *ApiServer) configureRouter() {
-	s.router.HandleFunc("/hello", s.handleHello())
-}
-
-func (s *ApiServer) configureStore() error {
-	st := store.New(s.config.Store)
-	if err := st.Open(); err != nil {
-		return err
+func newDB(databaseUrl string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", databaseUrl)
+	if err != nil {
+		return nil, err
 	}
-	s.store = st
 
-	return nil
-}
-
-func (s *ApiServer) handleHello() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello")
+	if err := db.Ping(); err != nil {
+		return nil, err
 	}
+
+	return db, nil
 }
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,17 +1,14 @@
 package apiserver
 
-import "github.com/bulbetski/learn_http-rest-api/internal/app/store"
-
 type Config struct {
-	BindAddr string `toml:"bind_addr"`
-	Loglevel string `toml:"log_level"`
-	Store    *store.Config
+	BindAddr    string `toml:"bind_addr"`
+	Loglevel    string `toml:"log_level"`
+	DatabaseUrl string `toml:"database_url"`
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
 		Loglevel: "debug",
-		Store:    store.NewConfig(),
 	}
 }
